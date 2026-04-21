4a2a4d44111f7143a606fd900f88a804cd4193cc
Maul-Goat <achmadmaulanaxx@gmail.com>
2026-04-21 23:38:20 +0700

    Update login.js

--- vercel/api/login.js
+++ vercel/api/login.js
@@ -1,7 +1,9 @@
 const { normalizeUsername, getClientIp } = require('../lib/auth');
 const { getUserByUsername, isDeviceBanned, upsertUserLogin, insertLog } = require('../lib/db');
+const { applyCors } = require('../lib/http');
 
 module.exports = async function handler(req, res) {
+  if (applyCors(req, res)) return;
   if (req.method !== 'POST') return res.status(405).json({ success: false, error: 'Method not allowed' });
 
   try {
